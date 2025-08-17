feb2a057ae071c613b217de4a8875c470815e559
Lacodernaunidh <naunidhsinghmadhok2023@gmail.com>
2025-08-17 14:34:06 +0530

    Added new code changes in Initiatives branch

--- src/pages/Initiatives.jsx
+++ src/pages/Initiatives.jsx
@@ -12,15 +12,77 @@ function Initiatives() {
     }
   };
 
-  // Minimal change: generate lorem
-  const lorem = "Lorem ipsum dolor sit amet consectetur adipiscing elit Fusce sed mauris vitae turpis sodales ullamcorper Integer in urna nec elit condimentum rhoncus Nullam at risus vel metus pharetra viverra in vitae nunc";
+  const lorem =
+    "Lorem ipsum dolor sit amet consectetur adipiscing elit Fusce sed mauris vitae turpis sodales ullamcorper Integer in urna nec elit condimentum rhoncus Nullam at risus vel metus pharetra viverra in vitae nunc";
 
   let data = [
-    { id: 1, title: "Event Wardha", desc: lorem, img: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=600" },
-    { id: 2, title: "Event Sindhri", desc: lorem, img: "https://images.unsplash.com/photo-1508614999368-9260051291ea?w=600" },
-    { id: 3, title: "Event Samvedna", desc: lorem, img: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=600" },
-    { id: 4, title: "Event Tirupati", desc: lorem, img: "https://images.unsplash.com/photo-1551836022-4c4c79ecde51?w=600" },
-    { id: 5, title: "Event Gaya", desc: lorem, img: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=600" }
+    {
+      id: 1,
+      title: "LiGHT Wardha Event",
+      desc: "This event was conducted to raise funds to provide clean water facilities to children in Sankalp Orphanage in Wardha.",
+      details: [
+        "NO OF VOLUNTEERS WHO PARTICIPATED IN THE DRIVE: 150+",
+        "NO OF REGIONS COVERED IN THE EVENT: 11+",
+      ],
+      img: "/initiativeimages/Wardha ppt.jpg",
+    },
+    {
+      id: 2,
+      title: "LiGHT Akola Event",
+      desc: "The event was conducted to raise money to provide a Volleyball court, and books for the library of a school named Dr. S R Patil School.",
+      details: [
+        "TOTAL NUMBER OF CHILDREN BENEFITED : 500+",
+        "TOTAL FUNDS RAISED IN THE EVENT : 8,125",
+      ],
+      img: "/initiativeimages/akolaevent.jpg",
+    },
+    {
+      id: 3,
+      title: "LiGHT Sindri Event",
+      desc: "The event was based on Quality education and the strategies to tackle the challenges in current education system.",
+      details: [
+        "NUMBER OF TEACHERS ATTENDED : 40+",
+        "TOTAL BENEFICIARIES INCLUDING STUDENTS : 70+",
+      ],
+      img: "/initiativeimages/sindrievent.jpg",
+    },
+    {
+      id: 4,
+      title: "LiGHT Raipur Event",
+      desc: "This event was conducted to raise money to donate water cooler and table fans to Kopal Vani Child Welfare Organisation in Raipur.",
+      details: [
+        "TOTAL NUMBER OF CHILDREN BENEFITED : 100+",
+        "TOTAL FUNDS RAISED IN THE EVENT : 6771",
+      ],
+      img: "/initiativeimages/raipurevent.jpg",
+    },
+    {
+      id: 5,
+      title: "LiGHT Samvedna Event",
+      desc: "The program focused on providing school uniforms to the children of Bhagwaan Baba Balika Ashram Orphanage.",
+      details: [
+        "TOTAL NUMBER OF CHILDREN BENEFITED : 25",
+        "TOTAL FUNDS RAISED IN THE EVENT : 12,500",
+      ],
+      img: "/initiativeimages/samvedna event.jpg",
+    },
+       {
+      id: 6,
+      title: "LiGHT Gwalior Thatipur",
+      desc: "The event was conducted to raise money to help underprivileged students and school by distributing blackboards, dustbins and waterfilter.",
+      details: [
+        "TOTAL NUMBER OF CHILDREN BENEFITED : 250+",
+        "TOTAL FUNDS RAISED IN THE EVENT : 5848",
+      ],
+      img: "/initiativeimages/gwaliorthathipurevent.jpg",
+    },
+
+    {
+      id: 7,
+      title: "Event Gaya",
+      desc: lorem,
+      img: "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?w=600",
+    },
   ];
 
   return (
@@ -30,9 +92,24 @@ function Initiatives() {
         <button onClick={() => s("l")}>&lt;</button>
         <div style={{ display: "flex", overflowX: "auto" }} ref={sc}>
           {data.map((e) => (
-            <div key={e.id} style={{ minWidth: "300px", border: "1px solid #ccc", margin: "10px" }}>
-              <img src={e.img} style={{ width: "100%" }} />
+            <div
+              key={e.id}
+              style={{
+                minWidth: "300px",
+                border: "1px solid #ccc",
+                margin: "10px",
+                padding: "10px",
+                borderRadius: "10px",
+              }}
+            >
+              <img src={e.img} style={{ width: "100%", borderRadius: "10px" }} />
               <h2>{e.title}</h2>
+              {e.details &&
+                e.details.map((d, i) => (
+                  <p key={i}>
+                    <b>📌 {d}</b>
+                  </p>
+                ))}
               <p>{e.desc}</p>
             </div>
           ))}
