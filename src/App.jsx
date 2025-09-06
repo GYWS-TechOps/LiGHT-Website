fc13b887deb74f726d7cf93d9380bd43ab2a3f0d
Lacodernaunidh <naunidhsinghmadhok2023@gmail.com>
2025-09-06 20:23:06 +0530

    Merged Home into ArkaLight and resolved conflicts using Home version

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Aboutus from './pages/Aboutus';
+import Centers from './pages/Centers';
+import Contactus from './pages/Contactus';
+import Initiatives from './pages/Initiatives';
+import JoinUs from './pages/JoinUs';
+import Members from './pages/Members';
 
-import Navbar from "./components/Navbar";
-
-import Home from "./pages/Home";
-import Aboutus from "./pages/Aboutus";
-import Centers from "./pages/Centers";
-import Contactus from "./pages/Contactus";
-import Initiatives from "./pages/Initiatives";
-import JoinUs from "./pages/JoinUs";
-import Members from "./pages/Members";
-import Footer from "./components/Footer";
-
-const App = () => {
+function App() {
   return (
     <Router>
       <Navbar />
-<<<<<<< HEAD
-      <div className="p-0">
-=======
-      <div >
->>>>>>> origin/Home
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<Aboutus />} />
-          <Route path="/centers" element={<Centers />} />
-          <Route path="/contact" element={<Contactus />} />
-          <Route path="/initiatives" element={<Initiatives />} />
-          <Route path="/join" element={<JoinUs />} />
-          <Route path="/members" element={<Members />} />
-        </Routes>
-      </div>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<Aboutus />} />
+        <Route path='/centers' element={<Centers />} />
+        <Route path='/contact' element={<Contactus />} />
+        <Route path='/initiatives' element={<Initiatives />} />
+        <Route path='/join' element={<JoinUs />} />
+        <Route path='/members' element={<Members />} />
+      </Routes>
     </Router>
   );
-};
+}
 
 export default App;
